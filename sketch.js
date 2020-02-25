44c8556abecd2a2ff4579fceed994ea0fe53e92d
motingzhi <57576789+motingzhi@users.noreply.github.com>
2020-02-25 12:23:34 +0100

    3

--- sketch.js
+++ sketch.js
@@ -12,11 +12,16 @@ let ell1;
 let value;
 let datax= [];
 let datay=[];
-let choosestation ;
+let choosestation =1;
 
+
+let switchyear=1;
 let value3;
 let value4;
 let value5;
+
+let maxD,minD,maxD2,minD2;
+
 const options = {
   lat:48.85,
   lng:2.35,
@@ -52,14 +57,21 @@ function setup() {
   myMap = mappa.tileMap(options);
   myMap.overlay(canvas);
  
-   let max=0;
-  let min= Infinity;
+   let max1=0;
+  let min1= Infinity;
   
+  
+   let max2=0;
+  let min2= Infinity;
    for ( let row of stations.rows)//csv表的每一行
   {
     let lat=row.get('Latitude');
     let lon =row.get('Longitude');
-    let numAccident =Number( row.get('accidentNumber2'));
+    
+       let numAccident =Number( row.get('accidentNumber2'));
+    let numAccident2 =Number( row.get('AccidentNumber'));
+      
+      
     let station = row.get('Station');
     let Top1 = row.get('Top1');
      let Top2 = row.get('Top2');
@@ -68,32 +80,38 @@ function setup() {
      lat,
      lon,
      numAccident,
+     numAccident2,
      station,
      Top1,
      Top2,
      Top3
    });
  
-    if(numAccident> max){
-      max = numAccident;
+    if(numAccident> max1){
+      max1 = numAccident;
     }
-    if(numAccident< min){
-      min = numAccident;
+    if(numAccident< min1){
+      min1 = numAccident;
+    }
+     if(numAccident2> max2){
+      max2 = numAccident2;
+    }
+    if(numAccident2< min2){
+      min2 = numAccident2;
     }
     
- 
+    
+  maxD = sqrt(max1);
+  minD = sqrt(min1);
+  
+      
+  maxD2 = sqrt(max2);
+  minD2 = sqrt(min2);
+  
    
   
   }
-   let maxD = sqrt(max);
-  let minD = sqrt(min);
   
-  for( let country of data)
-  {
-    country.diameter = map(sqrt(country.numAccident), minD,maxD,1,7);
-    country.diameter2 = map(sqrt(country.numAccident), minD,maxD,0.8,6);
-   
-  }
   
   
   
@@ -101,7 +119,23 @@ function setup() {
 }
 
 function draw() {
-  
+  if(switchyear==1){
+      for( let country of data)
+  {
+    country.diameter = map(sqrt(country.numAccident), minD,maxD,1,7);
+    country.diameter2 = map(sqrt(country.numAccident), minD,maxD,0.8,6);
+   
+  }
+     }
+  if(switchyear==2){
+     for( let country of data)
+  {
+    country.diameter = map(sqrt(country.numAccident2), minD2,maxD2,1,7);
+    country.diameter2 = map(sqrt(country.numAccident2), minD2,maxD2,0.8,6);
+   
+  }
+  }
+ 
          
    clear();
 
@@ -188,13 +222,12 @@ function draw() {
     text(value, 60, 125,300, 100);
   
   
-     fill(255,0,200,rectalpha255);
-     rect(60,150,80,16);
-      
-     
-    rect(60,175,80,16);
- 
-  rect(60,200,70,16);
+     fill(255,50,200,rectalpha255);
+     rect(60,150,80,16);  
+  fill(255,100,200,rectalpha255);
+    rect(60,175,70,16);
+  fill(255,150,200,rectalpha255);
+  rect(60,200,60,16);
   fill(0,0,0,rectalpha255);
    text(value4,150,175,400,200);
   text(value3,150,150,400,200);
@@ -203,51 +236,20 @@ function draw() {
   
 }
 
-// function  mousePressed() {
-//   datax=[];
-//   datay=[];
-//  let i = 0;
-//   for( let country of data){
-   
-//   const pix = myMap.latLngToPixel(country.lat, country.lon);
-   
-//     datax.push(
-//      pix.x,
-//    );
-//     datay.push(
-//     pix.y  
-//     )
-//     // console.log(country.station);
-//     //console.log(pix);
-//     // console.log(mouseX);
-  
-//     if( (pix.x-5)< mouseX && (pix.x+5)>mouseX)
-//     {
-//       if((pix.y-5)< mouseY && (pix.y+5)>mouseY)
-//          {
-         
-//          }
-     
-//     }
-//  }
-
-// //    for (var a = 0; a < 48;a = a+ 1) {
-// //  	if(datax[a]-1>mouseX)
-// //     {
-      
-// //     }
-// // }
-  
-// //   if ( mouse.X=== 0) {
-// //     value = 255;
-// //   } else {
-
-// //   }
-// }
 
 function  mousePressed() {
 //console.log("yes");
  let i = 0;
+  if( 35< mouseX && 55>mouseX)
+    {
+      if(290< mouseY && 310>mouseY)
+         {
+          
+            switchyear=2;
+        
+         }
+     
+    }
   for( let country of data){
    
   const pix = myMap.latLngToPixel(country.lat, country.lon);
